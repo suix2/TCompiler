92feaf95828b359cf55f17cb75677af70e6ac212
Xinyu Sui <suix2@uw.edu>
2016-08-17 11:27:09 +0800

    New Proj

---
+++ TCompiler/TCompiler.cpp
@@ -0,0 +1,13 @@
+// TCompiler.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	system("pause");
+    return 0;
+}
+
